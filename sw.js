2eef6f47078e2f3d1d7fd29999800f1e22fed45d
raucarc81 <rauca.rc.81@gmail.com>
2026-04-29 15:31:08 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'cal-policial-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg',
+  'https://unpkg.com/react@18.2.0/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone@7.23.5/babel.min.js',
+  'https://unpkg.com/recharts@2.8.0/umd/Recharts.js',
+];
+
+// Instalar: guarda todos los assets en caché
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Activar: limpia cachés antiguas
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: caché primero → sin conexión funciona igual
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(cached => cached || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }))
+      .catch(() => caches.match('./index.html'))
+  );
+});
