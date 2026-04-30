3f2ef02dc6713c28552a17524c5d8e08cddcdae3
raucarc81 <rauca.rc.81@gmail.com>
2026-04-30 09:54:40 +0200

    Update service worker cache version and syntax

--- sw.js
+++ sw.js
@@ -1,41 +1,16 @@
-const CACHE = 'cal-policial-v5';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon.svg',
-  'https://unpkg.com/react@18.2.0/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js',
-  'https://unpkg.com/@babel/standalone@7.23.5/babel.min.js',
-];
+const CACHE='cal-policial-v6';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 
-// Instalar: guarda todos los assets en caché
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-  );
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
 
-// Activar: limpia cachés antiguas
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
 
-// Fetch: caché primero → sin conexión funciona igual
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request)
-      .then(cached => cached || fetch(e.request).then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      }))
-      .catch(() => caches.match('./index.html'))
-  );
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(cached=>cached||fetch(e.request)));
 });
